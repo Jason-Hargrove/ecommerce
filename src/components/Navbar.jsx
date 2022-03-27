598c8faed641d2e4d09b251759b16d441fdb4e4a
Jason Hargrove <rijaha@gmail.com>
2022-03-27 13:13:50 -0500

    Added styled-components. Worked on Navbar.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+`;
+const Center = styled.div`
+    flex: 1;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+          <Left>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt obcaecati nisi nihil officiis dolorum tempora, aspernatur nostrum asperiores consequatur ex vero qui ad nam voluptatibus! Vitae nihil exercitationem impedit repellendus.</Left>
+          <Center>Center</Center>
+          <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
