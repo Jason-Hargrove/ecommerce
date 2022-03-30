f4e107bf6bd1395f69143f2969900bf779570452
Jason Hargrove <rijaha@gmail.com>
2022-03-29 21:57:44 -0500

    Finished initial styling of navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Search } from "@mui/icons-material";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import React from "react";
 import styled from "styled-components";
 
@@ -25,16 +27,16 @@ const Language = styled.span`
 `;
 
 const SearchContainer = styled.div`
-    border: 0.5px solid lightgray;
-    display: flex;
-    align-items: center;
-    margin-left: 25px;
-    padding: 5px;
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 `;
 
 const Input = styled.input`
   border: none;
-`
+`;
 
 const Center = styled.div`
   flex: 1;
@@ -43,10 +45,19 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
-`
+`;
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -54,20 +65,24 @@ const Navbar = () => {
     <Container>
       <Wrapper>
         <Left>
-          <Language>
-            EN
-          </Language>
+          <Language>EN</Language>
           <SearchContainer>
-              <Input/>
-              <Search/>
+            <Input />
+            <Search style={{color: "gray", fontSize: 16}}/>
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>
-            JASON.
-          </Logo>
+          <Logo>JASON.</Logo>
         </Center>
-        <Right>Right</Right>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
