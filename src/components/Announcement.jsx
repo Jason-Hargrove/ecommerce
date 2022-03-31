88254cf12983cbc583acad1b2c457c282b2db800
Jason Hargrove <rijaha@gmail.com>
2022-03-30 19:39:33 -0500

    Added a Container to Announcement and styled.

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+`
 
 const Announcement = () => {
   return (
     <div>
-      
+      <Container>
+          Super Deal! Free Shipping on Orders Over $50
+      </Container>
     </div>
   );
 }
