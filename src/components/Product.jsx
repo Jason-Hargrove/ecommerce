b75f98df0656675f20ba8218856f8e414f844244
Jason Hargrove <rijaha@gmail.com>
2022-04-14 07:55:55 -0500

    Setting up Product componets.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
-const Container = styled.div``
+const Container = styled.div``;
 
-const Product = () => {
+const Product = (item) => {
   return (
     <Container>
-
-
+      <Circle />
+      <Image src={item.img} />
+      <Info>
+        <Icon></Icon>
+      </Info>
     </Container>
   );
-}
+};
 
 export default Product;
