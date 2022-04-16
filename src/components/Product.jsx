912a2ad9619503bcd063e08da29c82c5276d083e
Jason Hargrove <rijaha@gmail.com>
2022-04-16 10:33:40 -0500

    Styling products.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,27 +1,66 @@
-import { ShoppingCartOutlined, Search, FavoriteBorderIcon } from "@mui/icons-material";
-// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-// import { Search } from "@mui/icons-material";
-// import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Search } from "@mui/icons-material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import styled from "styled-components";
 
 const Container = styled.div`
   flex: 1;
   margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
 `;
 
-const Circle = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Icon = styled.div``;
+const Circle = styled.div`
+width: 200px;
+height: 200px;
+border-radius: 50%;
+background-color: white;
+position: absolute;
+`;
 
-const Product = (item) => {
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Info = styled.div`
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+background-color: grey;
+z-index: 3;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+`;
+
+const Product = ({item}) => {
   return (
     <Container>
       <Circle />
       <Image src={item.img} />
       <Info>
         <Icon>
-          <ShoppingCartOutlined />
+          <ShoppingCartOutlinedIcon />
         </Icon>
         <Icon>
           <Search />
