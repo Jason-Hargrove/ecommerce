bfbc1c00edd60f20b9c914be813dc7309a998bf3
Jason Hargrove <rijaha@gmail.com>
2022-04-16 09:43:38 -0500

    Imported icons for mui.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,18 @@
+import { ShoppingCartOutlined, Search, FavoriteBorderIcon } from "@mui/icons-material";
+// import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+// import { Search } from "@mui/icons-material";
+// import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+`;
+
+const Circle = styled.div``;
+const Image = styled.img``;
+const Info = styled.div``;
+const Icon = styled.div``;
 
 const Product = (item) => {
   return (
@@ -8,7 +20,15 @@ const Product = (item) => {
       <Circle />
       <Image src={item.img} />
       <Info>
-        <Icon></Icon>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+        <Icon>
+          <Search />
+        </Icon>
+        <Icon>
+          <FavoriteBorderIcon />
+        </Icon>
       </Info>
     </Container>
   );
