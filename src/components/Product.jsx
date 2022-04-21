a3e15496cd743dcb4b95927bcf3a1aa2f64ce755
Jason Hargrove <rijaha@gmail.com>
2022-04-21 07:26:18 -0500

    Fixed overlay.

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,20 @@ import { Search } from "@mui/icons-material";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import styled from "styled-components";
 
+const Info = styled.div`
+opacity: 0;
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+background-color: rgba(0, 0, 0, 0.2);
+z-index: 3;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -13,6 +27,10 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
@@ -26,19 +44,6 @@ position: absolute;
 const Image = styled.img`
   height: 75%;
   z-index: 2;
-`;
-
-const Info = styled.div`
-width: 100%;
-height: 100%;
-position: absolute;
-top: 0;
-left: 0;
-background-color: grey;
-z-index: 3;
-display: flex;
-align-items: center;
-justify-content: center;
 `;
 
 const Icon = styled.div`
