9789752bf61486a6c1b28b498bdc43b668ab14a6
Jason Hargrove <rijaha@gmail.com>
2022-04-21 07:40:44 -0500

    Setup newsletter.

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,26 @@
+import SendIcon from '@mui/icons-material/Send';
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Title = styled.h1``;
+const Desc = styled.div``;
+const InputContainer = styled.div``;
+const Input = styled.input``;
+const Button = styled.button``;
+
+const Newsletter = () => {
+  return (
+    <Container>
+        <Title>Newsletter</Title>
+        <Desc>Get timely updates from your favorite products.</Desc>
+        <InputContainer>
+            <Input placeholder="Your email" />
+            <Button>
+                <SendIcon />
+            </Button>
+        </InputContainer>
+    </Container>
+  );
+}
+
+export default Newsletter;
