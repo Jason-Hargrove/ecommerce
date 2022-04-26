87cdd3bf3a3df6aa8cf31e2443498f1de308151b
Jason Hargrove <rijaha@gmail.com>
2022-04-26 07:27:39 -0500

    Making containers.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,50 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+display: flex;
+`;
+
+const Left = styled.div`
+flex: 1;
+`;
+
+const Center = styled.div`
+flex: 1;
+`;
+
+const Right = styled.div`
+flex: 1;
+`;
 
 const Footer = () => {
   return (
-    <Container></Container>
+    <Container>
+        <Left>
+            <Logo>LAMA.</Logo>
+            <Desc>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium harum quasi incidunt est consequuntur odit obcaecati eveniet nemo iste corporis. Atque quas rerum quidem labore, animi tenetur magnam possimus perspiciatis.
+            </Desc>
+            <SocialContainer>
+                <SocialIcon>
+                   <Facebook />
+                </SocialIcon>
+                <SocialIcon>
+                   <Facebook />
+                </SocialIcon>
+                <SocialIcon>
+                   <Facebook />
+                </SocialIcon>
+                <SocialIcon>
+                   <Facebook />
+                </SocialIcon>
+                <SocialIcon>
+                   <Facebook />
+                </SocialIcon>
+            </SocialContainer>
+        </Left>
+        <Center></Center>
+        <Right></Right>
+    </Container>
   )
 }
 
